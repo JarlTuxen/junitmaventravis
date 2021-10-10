ceb1df86f054eb7e71755c1aef0ffe7036db47c4
jarltuxen <jarl.tuxen@gmail.com>
2021-10-10 15:17:04 +0200

    initial junit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+    @BeforeEach
+    public void init(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void addTest(){
+        int expectedResult = 3;
+
+        int result = calculator.add(1, 2);
+
+        assertEquals(expectedResult, result);
+    }
+}
