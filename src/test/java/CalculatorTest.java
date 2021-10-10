13636b777af2342ea8d925f35bf3774248e8f111
jarltuxen <jarl.tuxen@gmail.com>
2021-10-10 16:38:27 +0200

    initial junit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,7 +13,7 @@ public class CalculatorTest {
 
     @Test
     public void addTest(){
-        int expectedResult = 3;
+        int expectedResult = 2;
 
         int result = calculator.add(1, 2);
 
